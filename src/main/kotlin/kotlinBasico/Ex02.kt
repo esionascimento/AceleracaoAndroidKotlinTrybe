cfb1352b42902335310c0ac3cd75f7f01f46199b
esionascimento <esionascimento@proton.me>
2024-02-13 19:15:52 -0400

    exercise 02 made from basic kotlin

---
+++ src/main/kotlin/kotlinBasico/Ex02.kt
@@ -0,0 +1,22 @@
+package com.betrybe.kotlinBasico
+
+/*
+Cada degrau de uma escada tem X de altura.
+Escreva um programa usando Kotlin que receba essa altura
+e a altura que uma pessoa deseja alcançar usando a escada,
+calcule e mostro quantos degraus ela deverá subir para atingir seu objetivo,
+sem se preocupar com a altura da pessoa.
+Todas as medidas fornecidas devem estar em metros.
+ */
+
+fun main() {
+    print("Digite a altura da escada: ")
+    val heightEscada: Double = readln().toDouble()
+
+    print("Digite a altura que deseja subir: ")
+    val heightSubir: Double = readln().toDouble()
+
+    val somaDegraus = (heightSubir / heightEscada).toInt()
+
+    println("Deverá ter $somaDegraus degraus")
+}
