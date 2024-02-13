e177a958c0054378e30218e490b23e30be515789
esionascimento <esionascimento@proton.me>
2024-02-13 18:57:58 -0400

    aula 01 conhecendo kotlin

---
+++ src/main/kotlin/aula01/App.kt
@@ -0,0 +1,8 @@
+package com.betrybe.aula01
+
+fun main() {
+    print("Digite seu nome: ");
+    var nome: String = readln()
+
+    println("Hello $nome")
+}
---
+++ src/main/kotlin/aula01/Ex01.kt
@@ -0,0 +1,27 @@
+package com.betrybe.aula01
+
+/*
+    Escrever um programa que recaba três números inteiros,
+    calcule e mostre a média aritmética simples entre os números
+ */
+
+fun main() {
+    print("Digite o primeiro número: ")
+    val num1: Int = readln().toInt()
+
+    print("Digite o segundo número: ")
+    val num2: Int = readln().toInt()
+
+    print("Digite o terceiro número: ")
+    val num3: Int = readln().toInt()
+
+    val media = calcularMedia(num1, num2, num3)
+    println("A média aritmetica simples é $media")
+}
+
+/*
+fun calcularMedia(a: Int, b: Int, c: Int): Int {
+    return ( a + b + c) / 3
+}*/
+
+fun calcularMedia(a: Int, b: Int, c: Int) = (a + b + c) / 3
